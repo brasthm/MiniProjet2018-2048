2368d8163898ba3613a3290be26040e23f07fda4
Cyril <brebisasthmatique@gmail.com>
2018-04-28 22:19:02 +0200

    Affichage de la grille et des blocs

--- src/grille.h
+++ src/grille.h
@@ -11,13 +11,20 @@
 class Grille
 {
 	private :
+		size_t pos_x_, pos_y_;
+
+		cimg_library::CImg<unsigned char> img_grille_;
+		std::vector<cimg_library::CImg<unsigned char>> img_blocs_;
+
 		std::vector<std::vector<int>> grille_;
+		
 	public:
 		Grille();
 		void deplacement(Direction dir);
-		void afficher();
+		void afficher(cimg_library::CImg<unsigned char> &scene);
 
 		void afficheConsole(); // Fonction de test
+		void initTestCouleurs();
 };
 
 #endif // !GRILLE_H
